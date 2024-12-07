8422ce6cdebc8217d7f9375aedd9f2f40baff041
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-12-07 14:44:31 +0200

    add error handling

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
+const AppError = require("./src/utils/appError");
+const globalErrorHandler = require("./src/controllers/errorController");
 
 const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+app.use(globalErrorHandler);
 
 module.exports = app;
