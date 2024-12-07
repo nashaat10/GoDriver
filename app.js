080e981913792760ed175bd20d5a5e2fc1660221
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-12-07 14:48:37 +0200

    add rate limiter

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+
 const AppError = require("./src/utils/appError");
 const globalErrorHandler = require("./src/controllers/errorController");
 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+
+app.use("/api", limiter);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
