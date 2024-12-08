330363f61fd2ba3e6e79bf9d08514271c98fc718
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-12-08 08:21:06 +0200

    add database connection

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const cors = require("cors");
 
 const AppError = require("./src/utils/appError");
 const globalErrorHandler = require("./src/controllers/errorController");
 
 const app = express();
-app.use(express.json());
-app.use(morgan("dev"));
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!",
 });
 
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 app.use("/api", limiter);
 
 app.all("*", (req, res, next) => {
--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE_URL;
+
+mongoose.connect(DB).then(() => {
+  console.log("Database connection successful");
+});
 
 const port = process.env.PORT || 5000;
 
