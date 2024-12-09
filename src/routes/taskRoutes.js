9af86104c49f7729888f57dcaa9899274c41f520
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-12-09 15:52:46 +0200

    Fix: merge conflict

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,15 +1,14 @@
-import express from 'express';
+import express from "express";
 // Import task controller methods
-import { 
+import {
   createTask,
   getAllTasksForManager,
   getAllTasksForDriver,
   getTaskById,
   updateTask,
-  deleteTask
-} from '../controllers/taskController.js'
-import { protect, restrictTo } from '../middlewares/authMiddleware.js';
-
+  deleteTask,
+} from "../controllers/taskController.js";
+import { protect, restrictTo } from "../middlewares/authMiddleware.js";
 
 // import { protect, restrictTo } from '../middlewares/authMiddleware'; // Middleware to protect routes and restrict access to certain roles
 
@@ -18,23 +17,23 @@ const router = express.Router();
 // Protect all routes in this router
 // router.use(protect);
 
-router.use(protect)
+router.use(protect);
 // Routes for **Managers**
 router
-  .route('/')
-  .post(restrictTo('manager'), createTask) // Only managers can create tasks
-  .get(restrictTo('manager'), getAllTasksForManager); // Only managers can view tasks they created
+  .route("/")
+  .post(restrictTo("manager"), createTask) // Only managers can create tasks
+  .get(restrictTo("manager"), getAllTasksForManager); // Only managers can view tasks they created
 
 // Routes for **Drivers**
 router
-  .route('/driver/:id')
-  .get(restrictTo('driver', 'manager'), getAllTasksForDriver); // Both drivers and managers can view tasks assigned to the driver
+  .route("/driver/:id")
+  .get(restrictTo("driver", "manager"), getAllTasksForDriver); // Both drivers and managers can view tasks assigned to the driver
 
 // Routes for specific task by ID
 router
-  .route('/:id')
-  .get(restrictTo('manager', 'driver'), getTaskById) // Both managers and drivers can view a specific task
-  .patch(restrictTo('manager'), updateTask) // Only managers can update tasks
-  .delete(restrictTo('manager'), deleteTask); // Only managers can delete tasks
+  .route("/:id")
+  .get(restrictTo("manager", "driver"), getTaskById) // Both managers and drivers can view a specific task
+  .patch(restrictTo("manager"), updateTask) // Only managers can update tasks
+  .delete(restrictTo("manager"), deleteTask); // Only managers can delete tasks
 
 export default router;
