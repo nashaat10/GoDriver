fe464f9709ec0977929854bc21e00a552ee19a26
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-12-10 15:13:59 +0200

    +update current user password

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,5 +5,10 @@ const router = express.Router();
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.get("/logout", authController.protect, authController.logout);
+router.patch(
+  "/updateMyPassword",
+  authController.protect,
+  authController.updatePassword
+);
 
 export default router;
