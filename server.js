b16cbee4b849a8573da238397336be4392509ed3
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-12-11 16:09:41 +0200

    Update: inform node env trigger

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ mongoose.connect(DB).then(() => {
 
 const port = process.env.PORT || 5000;
 
-
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(
+    `Server is running on port ${port} on ${process.env.NODE_ENV} mode`
+  );
 });
