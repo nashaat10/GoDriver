007e3f49bdfcbf74a91eb9f7133f3e8f41758863
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-12-11 14:59:25 +0200

    Update: commit redis connection for now

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import AppError from "./src/utils/appError.js";
 import globalErrorHandler from "./src/controllers/errorController.js";
 import taskRoutes from "./src/routes/taskRoutes.js";
-import redisClient from "./src/config/redis.js";
+// import redisClient from "./src/config/redis.js";
 import vehicleRoutes from "./src/routes/vehicleRoutes.js";
 import authRoutes from "./src/routes/authRoutes.js";
 import userRoutes from "./src/routes/userRoutes.js";
@@ -30,7 +30,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/manager", userRoutes);
 app.use("/api/v1/driver", userRoutes);
 // Redis connection
-redisClient.connect();
+// redisClient.connect();
 
 // Sockets
 
