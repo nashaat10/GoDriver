0aa67a21d560c6b3f2a4fc28a9acd0bb4f0e12c8
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-12-18 10:49:39 +0200

    send otp via email

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,6 +5,9 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { promisify } from "util";
 import sendEmail from "../utils/email.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../../config.env" });
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -125,7 +128,6 @@ export const updatePassword = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
-// implement forget and reset password with email verification and verification code
 export const forgetPassword = catchAsync(async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
@@ -133,24 +135,26 @@ export const forgetPassword = catchAsync(async (req, res, next) => {
   }
   const verificationCode = user.createVerificationCode();
   await user.save({ validateBeforeSave: false });
-  const resetURL = `${req.protocol}://${req.get(
-    "host"
-  )}/api/v1/users/resetPassword/${verificationCode}`;
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+
+  const message = `Forgot your password? Use the following verification code to reset your password: ${verificationCode}.\nIf you didn't forget your password, please ignore this email!`;
+
   try {
-    // await sendEmail({
-    //   email: user.email,
-    //   subject: "Your password verification code (valid for 10 min)",
-    //   message,
-    // });
+    await sendEmail({
+      from: process.env.EMAIL_FROM,
+      to: user.email,
+      subject: "Your password verification code (valid for 10 min)",
+      message,
+      otp: verificationCode,
+    });
     res.status(200).json({
       status: "success",
-      message: "verification code sent to email!",
+      message: "Verification code sent to email!",
     });
   } catch (err) {
     user.verificationCode = undefined;
     user.verificationCodeExpires = undefined;
     await user.save({ validateBeforeSave: false });
+    console.log(err);
     return next(
       new AppError("There was an error sending the email. Try again later!"),
       500
