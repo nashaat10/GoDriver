c37203a1207a16289f848baf28f734a77906ba11
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2024-12-19 10:00:46 +0200

    coordinates to bahrain

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,24 +1,49 @@
-import { Server } from 'socket.io';
-import http from 'http';
+import amqp from "amqplib/callback_api";
+import { Server } from "socket.io";
+import http from "http";
 import app from './app.js';
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
+export const io = new Server(server, {
     cors: {
-        origin: "*", // Configure according to your needs
+        origin: "*",
         methods: ["GET", "POST"]
     }
-
-
 });
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
     console.log('Client connected');
+
+    amqp.connect('amqp://localhost', (err0, connection) => {
+        if (err0) {
+            throw err0;
+        }
+        connection.createChannel((err1, channel) => {
+            if (err1) {
+                throw err1;
+            }
+            const alertQueue = 'alerts';
+            channel.assertQueue(alertQueue);
+
+            // Consume alert messages from the alert queue
+            channel.consume(alertQueue, (msg) => {
+                if (msg !== null) {
+                    const alertData = JSON.parse(msg.content.toString());
+                    console.log("Received Alert Data:", alertData);
+
+                    // Emit the alert through sockets
+                    io.emit('alert', alertData);
+                }
+            }, { noAck: true });
+        });
+    });
 
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
 });
 
-server.listen(3000)
+server.listen(4000, () => {
+    console.log('Socket server listening on port 4000');
+});
