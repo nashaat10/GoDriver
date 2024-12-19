133a25e9fe2ef2a45e825bb374873ba2b7d0f4a2
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2024-12-19 11:52:18 +0200

    alertsRoutes

--- src/routes/alertsRoutes.js
+++ src/routes/alertsRoutes.js
@@ -1,76 +1,14 @@
 import express from "express";
-import { Alert } from "../models/vehicleData.js";
-import catchAsync from "../utils/catchAsync.js";
-import Vehicle from '../models/vehicleModel.js';
-
+import { getAllAlerts, getAlertsByType , getAlertsByVehicle} from "../controllers/alertController.js";
 const router = express.Router();
 
 // Get all alerts
-router.get("/", catchAsync(async (req, res) => {
-    const alerts = await Alert
-        .find()
-        .populate('driverId', 'name')
-        .populate('vehicleId', 'brand model')
-        .sort('-alertTime');
-
-    res.status(200).json({
-        status: 'success',
-        results: alerts.length,
-        data: {
-            alerts
-        }
-    });
-}));
-
+router.get("/", getAllAlerts);
 // Get alerts by vehicle
-router.get("/:id", catchAsync(async (req, res) => {
-    const alerts = await Alert.find({ vehicleId: req.params.id })
-        .populate('driverId', 'name')
-        .populate('vehicleId', 'brand model')
-        .sort('-alertTime');
-
-    if (!alerts.length) {
-        return res.status(404).json({ message: 'No alerts found for this vehicle' });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            alerts
-        }
-    });
-}));
+router.get("/:id", getAlertsByVehicle);
 
 // Get alerts by type
-router.get('/type/:alertType', catchAsync(async (req, res) => {
-    const alerts = await Alert.find({ alertType: req.params.alertType })
-        .populate('driverId', 'name')
-        .populate('vehicleId', 'brand model')
-        .sort('-alertTime');
-
-    res.status(200).json({
-        status: 'success',
-        results: alerts.length,
-        data: {
-            alerts
-        }
-    });
-}));
-
-router.post("/trigger-alert", (req, res) => {
-    const alertData = req.body; // Assume alert data is sent in the request body
-
-    // Emit the alert through sockets
-    io.emit("alert", alertData, (ack) => {
-      if (ack) {
-        console.log("Alert sent successfully:", alertData);
-        res.status(200).json({ status: "success", message: "Alert sent successfully" });
-      } else {
-        console.error("Failed to send alert:", alertData);
-        res.status(500).json({ status: "error", message: "Failed to send alert" });
-      }
-    });
-  });
+router.get('/type/:alertType', getAlertsByType);
 
 
 export default router;
