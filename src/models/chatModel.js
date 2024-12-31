351e09eeffb08347dfb44d76b81ec53211300462
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-12-31 10:20:34 +0200

    add chat schema

--- src/models/chatModel.js
+++ src/models/chatModel.js
@@ -3,37 +3,44 @@ import mongoose from "mongoose";
 const messageSchema = new mongoose.Schema({
   sender: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+    ref: "User",
+    required: true,
   },
   content: {
     type: String,
-    required: true
+    required: true,
   },
   timestamp: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   read: {
     type: Boolean,
-    default: false
-  }
+    default: false,
+  },
 });
 
-const chatSchema = new mongoose.Schema({
-  participants: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  }],
-  messages: [messageSchema],
-  lastMessage: {
-    type: Date,
-    default: Date.now
+const chatSchema = new mongoose.Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    messages: [messageSchema],
+    lastMessage: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
-const Chat = mongoose.model('Chat', chatSchema);
-export default Chat;
+const Chat = mongoose.model("Chat", chatSchema);
+const Message = mongoose.model("Message", messageSchema);
+
+export default { Chat, Message };
