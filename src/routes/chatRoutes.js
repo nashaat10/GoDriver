f2a4b08bd22e14a9828182f0d7a7b4044b1152a3
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2025-01-08 10:57:27 +0200

    chat route

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,3 +1,14 @@
 import express from "express";
 import * as chatController from "../controllers/chatController.js"
 import {protect} from "../controllers/authController.js"
+
+const router = express.Router();
+
+router.use(protect);
+router.post("/", chatController.createChat);
+router.post("/send",chatController.sendMessages);
+router.get("/:chatId", chatController.getChatHistory);
+
+
+export default router;
+
