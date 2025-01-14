638dc5c383cbe14d66eababd2e66e632517c7fc3
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2025-01-14 11:19:13 +0200

    update task

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { sendNotification } from "../controllers/notificationController.js";
+import { protect } from "../controllers/authController.js";
 
 const router = express.Router();
 
+router.use(protect);
 router.post("/send-notification", sendNotification);
 
 export default router;
